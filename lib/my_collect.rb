a5f831fd2b24d73bbfae2c89d2c711d8c6805b7d
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-22 22:24:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,5 @@
 def my_collect(array)
+<<<<<<< HEAD
   i = 0
   empty_array = []
     while i < array.length
@@ -6,5 +7,15 @@ def my_collect(array)
       i += 1
     end
   empty_array
+=======
+  empty_array = []
+  i = 0
+    while i < array.length
+      yield(array[i])
+      empty_array.push(array[i].upcase)
+      empty_array.push(array[i].split(" "))
+    end
+  return empty_array
+>>>>>>> 48c12c5238733c63a4f22bd88d6247297773ec37
 end
 
